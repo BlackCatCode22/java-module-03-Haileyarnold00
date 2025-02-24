fe919caf9d3b6ec16d3a8c6359d43f00289c9243
Hailey Arnold <0989987@my.scccd.edu>
2025-02-23 19:07:33 -0800

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,13 @@
+public class Dog {
+    public static void main(String[] args) {
+
+        System.out.println(Cat.getCatCount());
+
+        Cat myCat = new Cat();
+        myCat.meow();
+        myCat.name = "Stella";
+        myCat.age = 8;
+        System.out.println(Cat.MAX_LIVES);
+        System.out.println(Cat.getCatCount());
+    }
+}
